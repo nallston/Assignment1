d5191b86ad4d3414c27081d03c9bfdb090e00905
nallston <nallst91@gmail.com>
2018-09-04 20:55:34 -0400

    Add files via upload

--- server.js
+++ server.js
@@ -1,26 +1,61 @@
-var http = require('http'), 
-    fs = require('fs'), 
+var http = require('http'),
+    fs = require('fs'),
     url = require('url'),
     port = 8080;
 
 /* Global variables */
-var listingData, server;
+
 
 var requestHandler = function(request, response) {
-  var parsedUrl = url.parse(request.url);
 
-  /*
-    Your request handler should send listingData in the JSON format if a GET request 
-    is sent to the '/listings' path. Otherwise, it should send a 404 error. 
+    //console.log((request));
+    //response.end('Server up');
 
-    HINT: explore the request object and its properties 
-    http://stackoverflow.com/questions/17251553/nodejs-request-object-documentation
-   */
+
+    var parsedUrl = url.parse(request.url);
+
+    console.log(parsedUrl.pathname);
+
+    if(parsedUrl.path == '/listings'){
+        response.end(JSON.stringify(listings));
+    }
+    else{
+        response.statusCode= 404;
+        response.end('Bad gateway error');
+    };
+    /*
+      Your request handler should send listingData in the JSON format if a GET request
+      is sent to the '/listings' path. Otherwise, it should send a 404 error.
+
+      HINT: explore the request object and its properties
+      http://stackoverflow.com/questions/17251553/nodejs-request-object-documentation
+     */
 };
 
+
+var server = http.createServer(requestHandler);
+var listingData, server, listings;
+
 fs.readFile('listings.json', 'utf8', function(err, data) {
-  /*
-    This callback function should save the data in the listingData variable, 
-    then start the server. 
-   */
+
+    listings = JSON.parse(data);
+
+
+    server.listen(port, function(){
+        console.log('Server is listening on: http://localhost:' + port);
+    });
+
+
+    // console.log(listings);
+    //console.log('Server listening on: http://127.0.0.1:' + port);
+
+    /*
+      This callback function should save the data in the listingData variable,
+      then start the server.
+     */
 });
+
+
+
+
+
